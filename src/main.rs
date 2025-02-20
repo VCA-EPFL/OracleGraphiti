c4a9f4006850fd449587e98ab07c3960e9bdf65e
Yann Herklotz <git@yannherklotz.com>
2025-02-20 16:34:25 +0100

    Add direction to the rewrites

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ use symbolic_expressions::Sexp;
 
 fn get_rules() -> Vec<Rewrite<SymbolLang, ()>> {
     vec![
-        rw!("L"; "(join ?s1 ?x (join ?s2 ?y ?z))" => "(join ?s2 (join ?s1 ?x ?y) ?z)"),
-        rw!("R"; "(join ?s1 (join ?s2 ?x ?y) ?z)" => "(join ?s2 ?x (join ?s1 ?y ?z))"),
+        rw!("L"; "(join ?s1 ?x (join ?s2 ?y ?z))" => "(pure ?s2 (join ?s2 (join ?s1 ?x ?y) ?z))"),
+        rw!("R"; "(join ?s1 (join ?s2 ?x ?y) ?z)" => "(pure ?s2 (join ?s2 ?x (join ?s1 ?y ?z)))"),
         rw!("E"; "(join ?s1 (split1 ?s2 ?x) (split2 ?s3 ?x))" => "?x"),
-        rw!("C"; "(join ?s1 ?x ?y)" => "(join ?s1 ?y ?x)"),
+        rw!("C"; "(join ?s1 ?x ?y)" => "(pure ?s1 (join ?s1 ?y ?x))"),
         rw!("PL"; "(join ?s1 (pure ?s2 ?x) ?y)" => "(pure ?s2 (join ?s1 ?x ?y))"),
         rw!("PR"; "(join ?s1 ?x (pure ?s2 ?y))" => "(pure ?s2 (join ?s1 ?x ?y))"),
         rw!("PE"; "(pure ?s1 (pure ?s2 ?x))" => "(pure ?s1 ?x)"),
@@ -21,45 +21,54 @@ fn get_rules() -> Vec<Rewrite<SymbolLang, ()>> {
 struct Step {
     rw: String,
     args: Vec<String>,
+    dir: bool
 }
 
-fn find_rewrite_term_and_rule(sexp1: &str, sexp2: &str) -> Option<(String,String)> {
+fn find_rewrite_term_and_rule(sexp1: &str, sexp2: &str) -> Option<(String,String,bool)> {
     let term1 = parse_str(sexp1).ok()?;
+    eprintln!("T1: {}", term1);
     let term2 = parse_str(sexp2).ok()?;
+    eprintln!("T2: {}", term2);
     
     // Find the Rewrite marker in the second term
-    let rewrite_pos = find_rewrite_position(&term2)?;
+    let (rewrite_pos, dir) = find_rewrite_position(&term2)?;
+    eprintln!("POS1: {:?}", rewrite_pos);
     
     // Get the original term from the first expression
     let result = get_term_at_position(&term1, &rewrite_pos)?;
+    eprintln!("POS2: {}", result);
     // And the Rewrite term from the second one, to get the rule name
-    let rw= get_term_at_position(&term2, &rewrite_pos)?;
+    let rw = get_term_at_position(&term2, &rewrite_pos)?;
+    eprintln!("POS3: {}", rw);
     match rw {
         Sexp::List(list) => {
-            return Some((result.to_string(), list[1].to_string()));
+            return Some((result.to_string(), list[1].to_string(), dir));
         }
         _ => panic!("Unexpected"),
     }
     
 }
 
-fn find_rewrite_position(sexp: &Sexp) -> Option<Vec<usize>> {
+fn find_rewrite_position(sexp: &Sexp) -> Option<(Vec<usize>, bool)> {
     match sexp {
         Sexp::List(list) => {
             // Check if this node is a rewrite marker
             if list.len() == 3 {
                 if let Sexp::String(first) = &list[0] {
                     if first == "Rewrite=>" {
-                        return Some(vec![]);
+                        return Some((vec![], true));
+                    }
+                    if first == "Rewrite<=" {
+                        return Some((vec![], false));
                     }
                 }
             }
             
             // Else search through subterms
             for (i, subterm) in list.iter().enumerate() {
-                if let Some(mut pos) = find_rewrite_position(subterm) {
+                if let Some((mut pos, b)) = find_rewrite_position(subterm) {
                     pos.insert(0, i);
-                    return Some(pos);
+                    return Some((pos, b));
                 }
             }
             None
@@ -153,13 +162,13 @@ fn explanation_to_steps(flat: &[FlatTerm<SymbolLang>]) -> Vec<Step> {
         let next = &pair[1];
 
         // println!("{}\n TRANSFORM\n {}", current, next);
-        let (matched,r_name) = find_rewrite_term_and_rule(&current.remove_rewrites().to_string(),&next.to_string()).unwrap();
+        let (matched,r_name,dir) = find_rewrite_term_and_rule(&current.remove_rewrites().to_string(),&next.to_string()).unwrap();
         // println!("\n MATCHED {}: {}", r_name, matched);
 
         // extract the arguments at the desired positions.
         let args = extract_args_from_sexps(&matched, &r_name);
 
-        steps.push(Step { rw: r_name, args });
+        steps.push(Step { rw: r_name, args, dir });
     }
     steps
 }
@@ -179,6 +188,7 @@ fn main() {
 
     let extractor = Extractor::new(&runner.egraph, AstSize);
     let (_cost, end_best) = extractor.find_best(runner.roots[0]);
+    // println!("{}", end_best);
     let mut explanation = runner
         .explain_equivalence(&expr, &end_best);
     let explanation_steps = explanation.make_flat_explanation();
